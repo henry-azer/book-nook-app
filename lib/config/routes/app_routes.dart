c6bcb18c14bf8ceda04030a83261a984d1f31c16
Henry-Azer <henryazer@outlook.com>
2022-11-12 01:18:31 +0200

    [IMPLEMENT] book nook configuration

---
+++ lib/config/routes/app_routes.dart
@@ -0,0 +1,43 @@
+import 'package:book_nook_app/injection_container.dart' as di;
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../core/utils/app_strings.dart';
+import '../../features/random_quote/presentation/cubit/random_quote_cubit.dart';
+import '../../features/random_quote/presentation/screens/quote_screen.dart';
+import '../../features/splash/presentation/screens/splash_screen.dart';
+
+class Routes {
+  static const String initialRoute = '/';
+  static const String randomQuoteRoute = '/randomQuote';
+}
+
+class AppRoutes {
+  static Route? onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.initialRoute:
+        return MaterialPageRoute(builder: (context) {
+          return const SplashScreen();
+        });
+
+      case Routes.randomQuoteRoute:
+        return MaterialPageRoute(builder: ((context) {
+          return BlocProvider(
+            create: ((context) => di.sl<RandomQuoteCubit>()),
+            child: const QuoteScreen(),
+          );
+        }));
+      default:
+        return undefinedRoute();
+    }
+  }
+
+  static Route<dynamic> undefinedRoute() {
+    return MaterialPageRoute(
+        builder: ((context) => const Scaffold(
+              body: Center(
+                child: Text(AppStrings.noRouteFound),
+              ),
+            )));
+  }
+}
---
+++ lib/config/themes/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../core/utils/app_colors.dart';
+import '../../core/utils/app_strings.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+      primaryColor: AppColors.primary,
+      hintColor: AppColors.hint,
+      brightness: Brightness.light,
+      scaffoldBackgroundColor: Colors.white,
+      fontFamily: AppStrings.fontFamily,
+      appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          color: Colors.transparent,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+              fontWeight: FontWeight.w500, color: Colors.black, fontSize: 20)),
+      textTheme: const TextTheme(
+        bodyMedium: TextStyle(
+            height: 1.3,
+            fontSize: 22,
+            color: Colors.white,
+            fontWeight: FontWeight.bold),
+      ));
+}
