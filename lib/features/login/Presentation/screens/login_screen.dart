e85e25654c2c693b00a764641216f80cc8ffe4ab
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2022-11-17 21:51:07 +0200

    Add new file

--- lib/features/login/Presentation/screens/login_screen.dart
+++ lib/features/login/Presentation/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:book_nook_app/core/utils/app_colors.dart';
+import 'package:book_nook_app/features/login/Presentation/widgets/Signin_Button.dart';
 import 'package:book_nook_app/features/login/Presentation/widgets/text_form_field_email.dart';
 import 'package:book_nook_app/features/login/Presentation/widgets/text_form_field_password.dart';
 import 'package:flutter/material.dart';
@@ -43,6 +44,10 @@ class _LoginScreenState extends State<LoginScreen> {
             height: 40,
           ),
           const Passtextform(),
+          const SizedBox(
+            height: 90,
+          ),
+          Signinbutton(),
         ],
       ),
     );
---
+++ lib/features/login/Presentation/widgets/Signin_Button.dart
@@ -0,0 +1,63 @@
+import 'package:book_nook_app/core/utils/app_colors.dart';
+import 'package:book_nook_app/features/login/Presentation/widgets/text_form_field_password.dart';
+import 'package:flutter/material.dart';
+
+class Signinbutton extends StatefulWidget {
+  const Signinbutton({Key? key}) : super(key: key);
+
+  @override
+  State<Signinbutton> createState() => _SigninbuttonState();
+}
+
+class _SigninbuttonState extends State<Signinbutton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 375,
+        height: 45,
+        child: Stack(
+          children: [
+            Center(
+              child: Container(
+                width: 330,
+                height: 47,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.textFormBorder,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(29),
+                      )),
+                  onPressed: () {},
+                  child: Text(
+                    "Sign in",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Barlow-Black.ttf'),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 50),
+              child: Container(
+                height: 120,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image.asset(
+                      'assets/images/Subtract.png',
+                      height: 120,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
+//ButtonStyle(
+//                   backgroundColor:MaterialStateProperty.all(AppColors.textFormBorder),
+//                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(29))
+//                 ),
