d53ebcbaa44397a0604fc94ef7513deec6e8265c
Tota <71022738+Tota6@users.noreply.github.com>
2022-11-18 00:56:48 +0200

    adding the main colors for splashing screen

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -5,4 +5,7 @@ import 'hex_color.dart';
 class AppColors {
   static Color primary = HexColor('#202053');
   static Color hint = Colors.grey;
+  static Color appBackgroundColor = HexColor('#000020');
+  static Color appBeigeColor = HexColor('#E8C39E');
+  static Color appLightBlueColor = HexColor('#2F2C79');
 }
