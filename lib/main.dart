26c24313ebb43f99d630c1908e0d0fcb719268fd
Tota <71022738+Tota6@users.noreply.github.com>
2022-11-18 00:56:00 +0200

    editing the way of starting the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 import 'app.dart';
+// import 'bloc_observer.dart';
 import 'bloc_observer.dart';
 import 'injection_container.dart' as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
-  BlocOverrides.runZoned(
-    () {
-      runApp(const QuoteApp());
-    },
-    blocObserver: AppBlocObserver(),
-  );
+  Bloc.observer = AppBlocObserver();
+  runApp(const QuoteApp());
 }
