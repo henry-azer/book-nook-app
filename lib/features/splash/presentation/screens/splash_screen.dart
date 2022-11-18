49de944c5ff913c60e9a56891842d8385e0b4c31
Tota <71022738+Tota6@users.noreply.github.com>
2022-11-18 03:19:58 +0200

    adding animation to the shapes of splash screen

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 
 import 'package:book_nook_app/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_animations/animation_builder/custom_animation_builder.dart';
+import 'package:simple_animations/animation_builder/play_animation_builder.dart';
 
 import '../../../../config/routes/app_routes.dart';
 import '../../../../core/utils/assets_manager.dart';
@@ -43,136 +45,184 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Center(
         child: Stack(
           children: [
-            Positioned(
-              top: -size.width / 4,
-              left: -size.width / 6,
-              child: Container(
-                width: size.width / 2.5,
-                height: size.width / 2,
-                decoration: BoxDecoration(
-                  color: AppColors.appBeigeColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 6,
-                        offset: const Offset(2, 7),
-                        color: const Color.fromRGBO(232, 195, 158, 1)
-                            .withOpacity(0.3))
-                  ],
-                ),
-              ),
+            CustomAnimationBuilder(
+              builder: (context, value, child) {
+                return Positioned(
+                  top: -value / 2,
+                  left: -value / 2.5,
+                  child: Container(
+                    width: size.width / 2.5,
+                    height: size.width / 2,
+                    decoration: BoxDecoration(
+                      color: AppColors.appBeigeColor,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 6,
+                            offset: const Offset(2, 7),
+                            color: const Color.fromRGBO(232, 195, 158, 1)
+                                .withOpacity(0.3))
+                      ],
+                    ),
+                  ),
+                );
+              },
+              duration: const Duration(seconds: 1),
+              tween: Tween(begin: 100.0, end: 200.0),
+              startPosition: 0,
             ),
-            Positioned(
-              top: size.width / 2.7,
-              left: -size.width / 4.5,
-              child: Container(
-                width: size.width / 1.5,
-                height: size.width / 1.5,
-                decoration: BoxDecoration(
-                  color: AppColors.appLightBlueColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 6,
-                        offset: const Offset(2, 7),
-                        color: AppColors.appLightBlueColor.withOpacity(0.3))
-                  ],
-                ),
-              ),
+            CustomAnimationBuilder(
+              builder: (context, value, child) {
+                return Positioned(
+                  top: value / 1.3,
+                  left: -value / 3.0,
+                  child: Container(
+                    width: size.width / 1.5,
+                    height: size.width / 1.5,
+                    decoration: BoxDecoration(
+                      color: AppColors.appLightBlueColor,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 6,
+                            offset: const Offset(2, 7),
+                            color: AppColors.appLightBlueColor.withOpacity(0.3))
+                      ],
+                    ),
+                  ),
+                );
+              },
+              duration: const Duration(seconds: 2),
+              tween: Tween(begin: 100.0, end: 200.0),
+              startPosition: 0,
             ),
-            Positioned(
-              top: size.width / 2,
-              left: -size.width / 50,
-              child: Container(
-                width: size.width / 5.5,
-                height: size.width / 1.5,
-                decoration: BoxDecoration(
-                  color: AppColors.appBeigeColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 6,
-                        offset: const Offset(2, 7),
-                        color: AppColors.appBeigeColor.withOpacity(0.3))
-                  ],
-                ),
-              ),
+            CustomAnimationBuilder(
+              builder: (BuildContext context, value, Widget? child) {
+                return Positioned(
+                  top: value / 1.1,
+                  left: -value / 40,
+                  child: Container(
+                    width: size.width / 5.5,
+                    height: size.width / 1.5,
+                    decoration: BoxDecoration(
+                      color: AppColors.appBeigeColor,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 6,
+                            offset: const Offset(2, 7),
+                            color: AppColors.appBeigeColor.withOpacity(0.3))
+                      ],
+                    ),
+                  ),
+                );
+              },
+              duration: const Duration(seconds: 2),
+              tween: Tween(begin: 100.0, end: 200.0),
+              startPosition: 0,
             ),
-            Positioned(
-              bottom: -size.width / 6,
-              left: -size.width / 9,
-              child: Container(
-                width: size.width / 2.5,
-                height: size.width / 2,
-                decoration: BoxDecoration(
-                  color: AppColors.appBeigeColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 6,
-                        offset: const Offset(2, 7),
-                        color: const Color.fromRGBO(232, 195, 158, 1)
-                            .withOpacity(0.3))
-                  ],
-                ),
-              ),
+            CustomAnimationBuilder(
+              builder: (BuildContext context, value, Widget? child) {
+                return Positioned(
+                  bottom: -value / 4,
+                  left: -value / 9,
+                  child: Container(
+                    width: size.width / 2.5,
+                    height: size.width / 2,
+                    decoration: BoxDecoration(
+                      color: AppColors.appBeigeColor,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 6,
+                            offset: const Offset(2, 7),
+                            color: const Color.fromRGBO(232, 195, 158, 1)
+                                .withOpacity(0.3))
+                      ],
+                    ),
+                  ),
+                );
+              },
+              duration: const Duration(seconds: 2),
+              tween: Tween(begin: 100.0, end: 200.0),
+              startPosition: 0,
             ),
-            Positioned(
-              bottom: size.width / 3.5,
-              right: -size.width / 4,
-              left: size.width / 25,
-              child: Container(
-                width: size.width / 5,
-                height: size.width / 3,
-                decoration: BoxDecoration(
-                  color: AppColors.appLightBlueColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 6,
-                        offset: const Offset(2, 7),
-                        color: AppColors.appLightBlueColor.withOpacity(0.3))
-                  ],
-                ),
-              ),
+            CustomAnimationBuilder(
+              builder: (BuildContext context, value, Widget? child) {
+                return Positioned(
+                  bottom: value / 1.5,
+                  right: -value / 1.5,
+                  left: value / 25,
+                  child: Container(
+                    width: size.width / 5,
+                    height: size.width / 3,
+                    decoration: BoxDecoration(
+                      color: AppColors.appLightBlueColor,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 6,
+                            offset: const Offset(2, 7),
+                            color: AppColors.appLightBlueColor.withOpacity(0.3))
+                      ],
+                    ),
+                  ),
+                );
+              },
+              duration: const Duration(seconds: 2),
+              tween: Tween(begin: 100.0, end: 200.0),
+              startPosition: 0,
             ),
-            Positioned(
-              bottom: size.width / 15,
-              right: -size.width / 1.5,
-              left: size.width / 25,
-              child: Container(
-                width: size.width / 2,
-                height: size.width / 10,
-                decoration: BoxDecoration(
-                  color: AppColors.appLightBlueColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 6,
-                        offset: const Offset(2, 7),
-                        color: AppColors.appLightBlueColor.withOpacity(0.3))
-                  ],
-                ),
-              ),
+            CustomAnimationBuilder(
+              builder: (BuildContext context, value, Widget? child) {
+                return Positioned(
+                  bottom: value / 6.5,
+                  right: -value / 10,
+                  child: Container(
+                    width: size.width / 2,
+                    height: size.width / 10,
+                    decoration: BoxDecoration(
+                      color: AppColors.appLightBlueColor,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 6,
+                            offset: const Offset(2, 7),
+                            color: AppColors.appLightBlueColor.withOpacity(0.3))
+                      ],
+                    ),
+                  ),
+                );
+              },
+              duration: const Duration(seconds: 2),
+              tween: Tween(begin: 100.0, end: 200.0),
+              startPosition: 0,
             ),
-            Positioned(
-              top: size.width / 3,
-              right: -size.width / 1.5,
-              left: size.width / 25,
-              child: Container(
-                width: size.width / 2.5,
-                height: size.width / 3.5,
-                decoration: BoxDecoration(
-                  color: AppColors.appBeigeColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 6,
-                        offset: const Offset(2, 7),
-                        color: AppColors.appLightBlueColor.withOpacity(0.3))
-                  ],
-                ),
-              ),
+            CustomAnimationBuilder(
+              builder: (BuildContext context, value, Widget? child) {
+                return Positioned(
+                  top: value / 2,
+                  right: -value / 6.5,
+                  // left: value / 25,
+                  child: Container(
+                    width: size.width / 2.5,
+                    height: size.width / 3.5,
+                    decoration: BoxDecoration(
+                      color: AppColors.appBeigeColor,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 6,
+                            offset: const Offset(2, 7),
+                            color: AppColors.appLightBlueColor.withOpacity(0.3))
+                      ],
+                    ),
+                  ),
+                );
+              },
+              duration: const Duration(seconds: 2),
+              tween: Tween(begin: 100.0, end: 200.0),
+              startPosition: 0,
             ),
             BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
