806bdf1e786ffef6b851197bb41bb9673e4b3163
henryazer <henry.azer@ntsal.com>
2022-11-19 22:39:15 +0200

    [Refactor] app utils

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -3,15 +3,19 @@ import 'package:flutter/material.dart';
 import 'hex_color.dart';
 
 class AppColors {
-  static Color primary = HexColor('#FFFFFF');
+  static Color primary = HexColor('#000020');
   static Color hint = HexColor('#FFFFFF');
 
+  static Color error = HexColor('#900C3F');
   static Color background = HexColor('#000020');
 
   static Color fontPrimary = HexColor('#FFFFFF');
   static Color fontSecondary = HexColor('#E8C39E');
 
-  static Color inputBorder = HexColor('#2F2C79');
+  static Color border = HexColor('#2F2C79');
 
   static Color button = HexColor('#2F2C79');
+
+  static Color black = HexColor('#000000');
+  static Color white = HexColor('#FFFFFF');
 }
