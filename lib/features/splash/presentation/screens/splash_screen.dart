d9adc96e50a8e30917fd7ad8d07b85a53e1b3c51
Tota <71022738+Tota6@users.noreply.github.com>
2022-11-19 19:30:54 +0200

    change the widgets name

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -2,15 +2,8 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:book_nook_app/core/utils/app_colors.dart';
-import 'package:book_nook_app/core/widgets/circle_widget.dart';
+import 'package:book_nook_app/core/widgets/circles_with_anitmation_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:im_animations/im_animations.dart';
-import 'package:simple_animations/animation_builder/custom_animation_builder.dart';
-import 'package:simple_animations/animation_builder/play_animation_builder.dart';
-import 'package:simple_animations/movie_tween/movie_tween.dart';
-
-import '../../../../config/routes/app_routes.dart';
-import '../../../../core/utils/assets_manager.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -47,49 +40,49 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Center(
         child: Stack(
           children: [
-            Circles(
+            CirclesWithAnimation(
               color: AppColors.appBeigeColor,
               height: 150,
               width: 150,
               topPostion: -50,
               leftPostion: -50,
             ),
-            Circles(
+            CirclesWithAnimation(
               color: AppColors.appLightBlueColor,
               width: 280,
               height: 280,
               topPostion: 150,
               leftPostion: -70,
             ),
-            Circles(
+            CirclesWithAnimation(
               color: AppColors.appBeigeColor,
               width: 90,
               height: 90,
               topPostion: 265,
               leftPostion: -10,
             ),
-            Circles(
+            CirclesWithAnimation(
               color: AppColors.appBeigeColor,
               width: 200,
               height: 200,
               topPostion: 550,
               leftPostion: -30,
             ),
-            Circles(
+            CirclesWithAnimation(
               color: AppColors.appBeigeColor,
               width: 150,
               height: 150,
               topPostion: 100,
               leftPostion: 280,
             ),
-            Circles(
+            CirclesWithAnimation(
               color: AppColors.appLightBlueColor,
               width: 170,
               height: 170,
               topPostion: 400,
               leftPostion: 160,
             ),
-            Circles(
+            CirclesWithAnimation(
               color: AppColors.appLightBlueColor,
               width: 50,
               height: 50,
