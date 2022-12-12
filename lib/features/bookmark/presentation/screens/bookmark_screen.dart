0877806b79eb757637e8a16eee46e34e0eaaac6e
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2022-12-12 18:50:55 +0200

    add file for new feature

---
+++ lib/features/bookmark/presentation/screens/bookmark_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BookMarkScreen extends StatefulWidget {
+  const BookMarkScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BookMarkScreen> createState() => _BookMarkScreenState();
+}
+
+class _BookMarkScreenState extends State<BookMarkScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+
+      ),
+    );
+  }
+}
