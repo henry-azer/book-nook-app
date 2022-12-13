9e981caff3a278e1c28b8a9313b5e5b00738558e
henryazer <henry.azer@ntsal.com>
2022-12-13 02:56:26 +0200

    [Refactor] screens routes and cubit providers

--- lib/app.dart
+++ lib/app.dart
@@ -4,7 +4,12 @@ import 'config/locale/app_localizations_setup.dart';
 import 'config/routes/app_routes.dart';
 import 'config/themes/app_theme.dart';
 import 'core/utils/app_strings.dart';
+import 'features/signin/presentation/cubit/signin_cubit.dart';
+import 'features/splash/presentation/cubit/app_welcome_cubit.dart';
 import 'features/splash/presentation/cubit/localization_cubit.dart';
+import 'features/user-profile/presentation/cubit/signout_cubit.dart';
+import 'features/user-profile/presentation/cubit/user_profile_cubit.dart';
+import 'features/user-recommended-books/presentation/cubit/recommended_books_cubit.dart';
 import 'injection_container.dart' as di;
 
 class BookNookApp extends StatelessWidget {
@@ -15,6 +20,11 @@ class BookNookApp extends StatelessWidget {
     return MultiBlocProvider(
         providers: [
           BlocProvider(create: (context) => di.sl<LocalizationCubit>()),
+          BlocProvider(create: ((context) => di.sl<AppWelcomeCubit>())),
+          BlocProvider(create: ((context) => di.sl<SigninCubit>())),
+          BlocProvider(create: ((context) => di.sl<SignoutCubit>())),
+          BlocProvider(create: ((context) => di.sl<UserProfileCubit>())),
+          BlocProvider(create: ((context) => di.sl<RecommendedBooksCubit>()))
         ],
         child: BlocBuilder<LocalizationCubit, LocalizationState>(
           buildWhen: (previousState, currentState) {
--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -1,44 +1,36 @@
 import 'package:book_nook_app/features/book-info/presentation/screens/book_info_screen.dart';
-import 'package:book_nook_app/features/bookmark/presentation/screens/bookmark_screen.dart';
-import 'package:book_nook_app/features/categories/presentation/screens/categories_screen.dart';
-import 'package:book_nook_app/features/signin/presentation/cubit/signin_cubit.dart';
 import 'package:book_nook_app/features/signin/presentation/screens/signin_screen.dart';
-import 'package:book_nook_app/features/signup/Presentation/cubit/signup_cubit.dart';
 import 'package:book_nook_app/features/signup/Presentation/screens/signup_account_info.dart';
 import 'package:book_nook_app/features/signup/Presentation/screens/signup_user_info.dart';
-import 'package:book_nook_app/features/splash/presentation/cubit/app_welcome_cubit.dart';
 import 'package:book_nook_app/features/splash/presentation/screens/splash_screen.dart';
-import 'package:book_nook_app/injection_container.dart' as di;
+import 'package:book_nook_app/features/user-rated-book/presentation/screens/user_rated_books_screen.dart';
+import 'package:book_nook_app/features/user-welcome/presentation/screens/user_welcome_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../core/utils/app_strings.dart';
-import '../../features/home/presentation/cubit/recommended_books_cubit.dart';
+import '../../features/book-categories/presentation/screens/book_categories_screen.dart';
 import '../../features/home/presentation/screens/home_screen.dart';
-import '../../features/home/presentation/screens/recommedned_books_screen.dart';
 import '../../features/splash/presentation/screens/welcome_screen.dart';
-import '../../features/user-profile/presentation/cubit/signout_cubit.dart';
-import '../../features/user-profile/presentation/cubit/user_profile_cubit.dart';
 import '../../features/user-profile/presentation/screens/user_profile_screen.dart';
+import '../../features/user-recommended-books/presentation/screens/user_recommended_books_screen.dart';
 
 class Routes {
   static const String initial = '/';
+  static const String appHome = '/app-home';
   static const String appWelcome = '/app-welcome';
+
   static const String signin = '/signin';
   static const String signupUserInfo = '/signup-user-info';
   static const String signupAccountInfo = '/signup-account-info';
 
-  // static const String beforehomepage = '/beforehomepage';
-
-  static const String userProfile = '/user-profile';
-
-  static const String home = '/app-home';
+  static const String booksCategories = '/books-categories';
   static const String booksSearch = '/books-search';
-  static const String booksRecommended = '/books-recommended';
-  static const String userRatedBooks = '/user-rated-books';
   static const String bookInfo = '/book-info';
-  static const String categories = '/categories';
-  static const String bookmark = '/bookmark';
+
+  static const String userRecommendedBooks = '/user-recommended-books';
+  static const String userRatedBooks = '/user-rated-books';
+  static const String userProfile = '/user-profile';
+  static const String userWelcome = '/user-welcome';
 }
 
 class AppRoutes {
@@ -46,39 +38,17 @@ class AppRoutes {
     switch (routeSettings.name) {
       case Routes.initial:
         return MaterialPageRoute(builder: (context,) {
-          return const BookMarkScreen();
+          return const SplashScreen();
         }, settings: routeSettings);
 
       case Routes.appWelcome:
         return MaterialPageRoute(builder: (context) {
-          return BlocProvider(
-            create: ((context) => di.sl<AppWelcomeCubit>()),
-            child: const WelcomeScreen(),
-          );
+          return const WelcomeScreen();
         }, settings: routeSettings);
 
       case Routes.signin:
         return MaterialPageRoute(builder: (context) {
-          return BlocProvider(
-            create: ((context) => di.sl<SigninCubit>()),
-            child: const SigninScreen(),
-          );
-        }, settings: routeSettings);
-
-      case Routes.bookmark:
-        return MaterialPageRoute(builder: (context) {
-          return BlocProvider(
-            create: ((context) => di.sl<SigninCubit>()),
-            child: const BookMarkScreen(),
-          );
-        }, settings: routeSettings);
-
-      case Routes.categories:
-        return MaterialPageRoute(builder: (context) {
-          return BlocProvider(
-            create: ((context) => di.sl<SigninCubit>()),
-            child: const CategoriesScreen(),
-          );
+          return const SigninScreen();
         }, settings: routeSettings);
 
       case Routes.signupUserInfo:
@@ -88,37 +58,22 @@ class AppRoutes {
 
       case Routes.signupAccountInfo:
         return MaterialPageRoute(builder: (context) {
-          return BlocProvider(
-            create: ((context) => di.sl<SignupCubit>()),
-            child: const SignupAccountInfoScreen(),
-          );
+          return const SignupAccountInfoScreen();
         }, settings: routeSettings);
 
-    // case Routes.beforehomepage:
-    //   return MaterialPageRoute(builder: (context) {
-    //     return BlocProvider(
-    //       create: ((context) => di.sl<SigninCubit>()),
-    //       child: const BeforeHomePageScreen(),
-    //     );
-    //   }, settings: routeSettings);
+    case Routes.userWelcome:
+      return MaterialPageRoute(builder: (context) {
+        return const UserWelcomeScreen();
+      }, settings: routeSettings);
 
-      case Routes.home:
+      case Routes.appHome:
         return MaterialPageRoute(builder: (context) {
-          return MultiBlocProvider(
-            providers: [
-              BlocProvider(create: ((context) => di.sl<UserProfileCubit>()),),
-              BlocProvider(create: ((context) => di.sl<RecommendedBooksCubit>()),)
-            ],
-            child: const HomeScreen(),
-          );
+          return const HomeScreen();
         }, settings: routeSettings);
 
-      case Routes.booksRecommended:
+      case Routes.userRecommendedBooks:
         return MaterialPageRoute(builder: (context) {
-          return BlocProvider(
-            create: ((context) => di.sl<RecommendedBooksCubit>()),
-            child: const RecommendedBooksScreen(),
-          );
+          return const UserRecommendedBooksScreen();
         }, settings: routeSettings);
 
       case Routes.bookInfo:
@@ -126,22 +81,20 @@ class AppRoutes {
           return const BookInfoScreen();
         }, settings: routeSettings);
 
+      case Routes.userRatedBooks:
+        return MaterialPageRoute(builder: (context) {
+          return const UserRatedBooksScreen();
+        }, settings: routeSettings);
+
+      case Routes.booksCategories:
+        return MaterialPageRoute(builder: (context) {
+          return const BookCategoriesScreen();
+        }, settings: routeSettings);
+
       case Routes.userProfile:
-        return MaterialPageRoute(
-            builder: (context) {
-              return MultiBlocProvider(
-                providers: [
-                  BlocProvider(
-                    create: ((context) => di.sl<UserProfileCubit>()),
-                  ),
-                  BlocProvider(
-                    create: ((context) => di.sl<SignoutCubit>()),
-                  )
-                ],
-                child: const UserProfileScreen(),
-              );
-            },
-            settings: routeSettings);
+        return MaterialPageRoute(builder: (context) {
+          return const UserProfileScreen();
+        }, settings: routeSettings);
 
       default:
         return undefinedRoute();
